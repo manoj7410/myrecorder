75f3aad448f87b1a4ce80c3a86860697620b5e6d
manoj <manoj.mca2012@hotmail.com>
2019-04-18 12:39:21 +0530

    Commiting code having timestamp

--- recordingCode/recordwithcv2.py
+++ recordingCode/recordwithcv2.py
@@ -1,27 +1,36 @@
 import numpy as np
 import cv2
 from time import sleep
-file_count = 0
+import datetime
+import os
+cwd = os.getcwd()
 cap = cv2.VideoCapture(0)
 while True:
+    if cap==True:
+        print "Camera is connected"
+    else:
+        print "Camera is not connected. Exiting!!"
+        break
     # Define the codec and create VideoWriter object
+    currentTime = datetime.datetime.now()
+    #Concat CWD and Timestamp to make it generic
+    fileName = "{0}/output{1}.avi".format(cwd,currentTime) 
+    fileName = fileName.replace(" ","") #Remove Spaces from Timestamp
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     frame_counter = 0
-    out = cv2.VideoWriter('/home/manoj/recordingCode/output%d.avi' % file_count,fourcc, 25.0, (640,480))
-    file_count += 1
+    out = cv2.VideoWriter(fileName,fourcc, 25.0, (640,480))
     while frame_counter <= 3000:
         ret, frame = cap.read()
         if ret==True:
-            frame = cv2.flip(frame,1)
+            # frame = cv2.flip(frame,2)
             frame_counter += 1
-            # write the flipped frame
+            # write the frame
             out.write(frame)
             # cv2.imshow('frame',frame)
             if frame_counter == 3000:
                 out.release()
         else:
             break
-
 # Release everything if job is finished
 cap.release()
 out.release()
